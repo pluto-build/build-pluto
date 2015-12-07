82e45201a7a20dcaa9da025107a336d999f0b537
Andre Pacak <andre.pacak@googlemail.com>
2015-12-07 13:13:19 +0100

    remove unused and not available import

--- src/build/pluto/buildpluto/TestSourceCode.java
+++ src/build/pluto/buildpluto/TestSourceCode.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import org.junit.runner.JUnitCore;
 import org.sugarj.common.Exec;
 import org.sugarj.common.Exec.ExecutionError;
 import org.sugarj.common.Exec.ExecutionResult;
